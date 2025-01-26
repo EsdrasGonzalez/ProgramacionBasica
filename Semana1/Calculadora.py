f6f5bed5e508d27b83e96bdc37448f287aeb4ec1
EsdrasGonzalez <3hgonzalezverbenaesdrasjesus@gmail.com>
2025-01-26 14:27:43 -0600

    Create Calculadora.py

---
+++ Semana1/Calculadora.py
@@ -0,0 +1,8 @@
+a=int(input("Ingresa el primer numero: "))
+b=int(input("Ingresa el segundo numero:"))
+print("Adiccion:", a+b)
+print("Sustraccion:", a-b)
+print("Multiplicacion:", a*b)
+print("Division:", a/b)
+
+
